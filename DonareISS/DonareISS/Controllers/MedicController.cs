0dc9f0cb764ec76f89d555be119b1a5bb6d3a44f
Robert Szacsko <robiiszacsko@gmail.com>
2018-04-23 19:06:29 +0300

    add controler medic + layout

---
+++ DonareISS/DonareISS/Controllers/MedicController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DonareISS;
+
+namespace DonareISS.Controllers
+{
+    public class MedicController : Controller
+    {
+        private ISSEntities db = new ISSEntities();
+
+        // GET: Medic
+        public ActionResult Index()
+        {
+            return View(db.Medic.ToList());
+        }
+
+        // GET: Medic/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medic medic = db.Medic.Find(id);
+            if (medic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medic);
+        }
+
+        // GET: Medic/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medic/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Medic,Spital")] Medic medic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Medic.Add(medic);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medic);
+        }
+
+        // GET: Medic/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medic medic = db.Medic.Find(id);
+            if (medic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medic);
+        }
+
+        // POST: Medic/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Medic,Spital")] Medic medic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medic).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(medic);
+        }
+
+        // GET: Medic/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medic medic = db.Medic.Find(id);
+            if (medic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medic);
+        }
+
+        // POST: Medic/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Medic medic = db.Medic.Find(id);
+            db.Medic.Remove(medic);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Pacienti(int? id)
+        {
+            Medic medic = db.Medic.Find(id);
+            return View(medic.Pacient);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
