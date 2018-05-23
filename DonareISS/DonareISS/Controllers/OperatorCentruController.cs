10f02df1be93c9dddd32a81a72e153a50c457aad
Robert Szacsko <robiiszacsko@gmail.com>
2018-05-23 11:13:55 +0300

    dasaSDASDs

--- DonareISS/DonareISS/Controllers/OperatorCentruController.cs
+++ DonareISS/DonareISS/Controllers/OperatorCentruController.cs
@@ -7,6 +7,9 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DonareISS;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using System.Web.Helpers;
 
 namespace DonareISS.Controllers
 {
@@ -18,6 +21,18 @@ namespace DonareISS.Controllers
         public ActionResult Index()
         {
             return View(db.OperatorCentru.ToList());
+        }
+
+        public JsonResult GetOperator()
+        {
+            var json = JsonConvert.SerializeObject(db.OperatorCentru.ToList(), Formatting.Indented,
+                        new JsonSerializerSettings()
+                        {
+                            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+
+                        });
+
+            return Json(json, JsonRequestBehavior.AllowGet);
         }
 
         // GET: OperatorCentru/Details/5
@@ -113,6 +128,55 @@ namespace DonareISS.Controllers
             db.OperatorCentru.Remove(operatorCentru);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [ChildActionOnly]
+        public ActionResult CerereNoua()
+        {
+            IEnumerable<Cerere> cerere = db.Cerere.Where(x => x.Status.ToLower() == "inregistrata");
+
+            if (cerere != null)
+                return View(cerere);
+            return View(new List<Cerere>());
+        }
+
+        public ActionResult EditCerere(int id)
+        {
+            return View(db.Cerere.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult EditCerere([Bind(Include = "Celula_Ceruta,Cantitate_Ceruta")] Cerere cerere)
+        {
+            var user = (Utilizator)Session["Utilizator"];
+
+            if (ModelState.IsValid)
+            {
+                cerere.Status = "procesata";
+                var stoc = db.Stoc.Where(x => x.OperatorCentru.Utilizator.Id_Utilizator == user.Id_Utilizator).First();
+                int numVal = Int32.Parse(cerere.Cantitate_Ceruta);
+
+                if (cerere.Celula_Ceruta.ToLower() == "plasma")
+                    stoc.CantitatePlasma = stoc.CantitatePlasma - numVal;
+                else if (cerere.Celula_Ceruta.ToLower() == "trombocite")
+                    stoc.CantitateTrombocite = stoc.CantitateTrombocite - numVal;
+                else if (cerere.Celula_Ceruta.ToLower() == "globulerosii")
+                    stoc.CantitateGlobuleRosii = stoc.CantitateGlobuleRosii - numVal;
+                db.SaveChanges();
+            }
+            return View("CerereProcesata", db.Cerere.Where(x => x.Cantitate_Ceruta == cerere.Cantitate_Ceruta && x.Celula_Ceruta == cerere.Celula_Ceruta).First().Medic);
+        }
+
+        [ChildActionOnly]
+        public ActionResult VeziStoc()
+        {
+            var user = (Utilizator)Session["Utilizator"];
+            var ope = db.OperatorCentru.Where(x => x.Utilizator.Id_Utilizator == user.Id_Utilizator).First();
+
+            if (ope.Stoc.First() != null)
+                return View(ope.Stoc.First());
+
+            return null;
         }
 
         protected override void Dispose(bool disposing)
